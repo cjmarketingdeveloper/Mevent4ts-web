28648b1c1e98f8ab11040c3e6d4c1294b5e3cc64
cjmarketingdeveloper <cjmarketingdeveloper@gmail.com>
2026-05-11 11:10:12 +0200

    Showing floor plan

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -3,6 +3,7 @@ export const API_URL          = "https://mevent-2-admin-c7b0d608277d.herokuapp.c
 export const API_BASE         = "https://mevent-2-admin-c7b0d608277d.herokuapp.com/";
 export const SESSION_COOKIE   = "MEVENTWEBtimewaistengnewLXd5dkplaytheball";
 
+
 /*
 export const API_URL          = "http://localhost:1077/api/";
 export const API_BASE         = "http://localhost:1077/";
--- src/Pages/HomeScreen.js
+++ src/Pages/HomeScreen.js
@@ -179,13 +179,10 @@ function HomeScreen() {
                     
                     <div className="w-50 p-2">
                         <div className="card-boxer">
-                          <img src={whiteFloor} className="home-card map-road" />
-                          {/*
-                          <button onClick={() => setShowModalDownloads(true)} className="btn btn-gray w-100 colorred">
-                            
+                          <img src={whiteFloor} className="home-card map-road" />                          
+                          <button onClick={() => setShowModalDownloads(true)} className="btn btn-gray w-100 colorred">                            
                               <span>View Map</span>
-                          </button>
-                          */}
+                          </button>                          
                         </div>
                     </div>
                   
@@ -198,7 +195,7 @@ function HomeScreen() {
                         </div>                        
                     </div>
                   </div>
-                  {/*
+                  
                   <div className="d-flex">
                       <div className="data-supplier-one">
                         <FaClipboard />
@@ -219,8 +216,8 @@ function HomeScreen() {
                         style={{ width: `${progressPercentage}%` }}
                       ></div>
                   </div>
-                  */
-                  }
+                  
+                  
                 </div>
               </div>
           </div>
