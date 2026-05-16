ca942c906f74c0a4edd4ff2e5037e32d1951b537
cjmarketingdeveloper <cjmarketingdeveloper@gmail.com>
2026-05-16 08:16:44 +0200

    Franchisee check happiness factor v5

--- src/Pages/HomeScreen.js
+++ src/Pages/HomeScreen.js
@@ -347,11 +347,12 @@ function HomeScreen() {
                   }
                 </>
             }
-            {user.profile.profileName}
+           
             {
               // (user.profile.profileName === "Franchisee") || (user.profile.profileName === "Potential") && 
               user.profile.profileName === "Franchisee" && 
               <>
+               {user.profile.profileName}
                   {
                     //indicateHappinessFactor && (
                    true && (                   
